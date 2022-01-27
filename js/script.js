86fca26c09fe80a5475c54ba04c1ee24118680d7
radfarelmira <radfar.elmira@gmail.com>
2022-01-27 16:02:41 +0100

    add select genre to index-json.html

--- js/script.js
+++ js/script.js
@@ -4,12 +4,19 @@ const app = new Vue({
     el: '#root',
     data: {
         discs: [],
+        selectedGenre: ''
     },
     methods: {
         getDiscsApi: function() {
-            axios.get('http://localhost:8888/php-ajax-dischi/server.php')
+            axios.get('http://localhost:8888/php-ajax-dischi/server.php',
+            {
+                params: {
+                    genre: this.selectedGenre
+                }
+            })
             .then((response) => {
-                this.discs = response.data
+                this.discs = response.data;
+                console.log(this.discs)
             });
         }
     },
--- server.php
+++ server.php
@@ -1,9 +1,29 @@
 <?php
 require __DIR__.'/database.php';
 
-$json= json_encode($database);
+$selected_genre = $_GET['genre'];
+
+if(empty($selected_genre)){
+
+    $json= json_encode($database);
+
+} else{
+
+    $filtered_discs = [];
+
+    foreach($database as $disc) {
+        
+        if($disc['genre'] === $selected_genre){
+            $filtered_discs = $disc;
+        }
+    }
+
+    $json = json_encode($filtered_discs);
+}
+
+
 
 header('Content-Type: application/json');
 
-echo $json
+echo $json;
 ?>
